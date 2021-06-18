fb9555d8b4f2ee5c35927a8fe25cdb5efd5cd41a
彩蛋 <1343460872@qq.com>
2021-06-18 14:16:07 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  publicPath: '/poker-game-demo/dist',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: true,
+  productionSourceMap: false,
   chainWebpack (config) {
-    config.module.rule('eslint').test(/\.(vue|(j|t)sx?)$/).pre()
-      .use('eslint-loader').loader('eslint-loader').tap(options => {
-        options.fix = true
-        return options
-      }).end()
+    config.module.rule('eslint').test(/\.(vue|(j|t)sx?)$/).pre().use('eslint-loader').loader('eslint-loader').tap(options => {
+      options.fix = true
+      return options
+    }).end()
   }
 }
